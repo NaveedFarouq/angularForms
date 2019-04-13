d8693919f304d43c7734bd5b5d5c5e43773cb81f
Naveed <naveedfarouq@hotmail.co.uk>
2019-04-13 14:48:08 +0100

    mouse events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularForm';
+  
+  counter = 0;
+
+  increment() {
+    this.counter++
+  }
+
+  decrement() {
+    // ifcounter is less than or equal to zero then let it be zero
+    // else decrement the value
+    this.counter <= 0 ? (this.counter = 0) : this.counter--;
+  }
+  
 }
