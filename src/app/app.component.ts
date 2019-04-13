e9ec89e5029849b2ee1f4f85c9719140ca245aa3
Naveed <naveedfarouq@hotmail.co.uk>
2019-04-13 14:54:31 +0100

    adding keyboard event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  keyPressed = false;
+  key = '';
   
   counter = 0;
 
@@ -17,6 +20,14 @@ export class AppComponent {
     // ifcounter is less than or equal to zero then let it be zero
     // else decrement the value
     this.counter <= 0 ? (this.counter = 0) : this.counter--;
+  }
+  showKey($event) {
+    this.keyPressed = true;
+    this.key = $event.key.toUpperCase();
+    setTimeout(() => {
+      this.keyPressed = false;
+    }, 500)
+    console.log(this.key)
   }
   
 }
